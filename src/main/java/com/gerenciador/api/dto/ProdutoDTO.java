e196a10d6905d2f99c4ee64ae1aa538b3083ab6b
stephanie sousa <sthersousa07@gmail.com>
2024-05-31 15:23:29 -0300

    Testando novo commit

--- src/main/java/com/gerenciador/api/dto/ProdutoDTO.java
+++ src/main/java/com/gerenciador/api/dto/ProdutoDTO.java
@@ -3,57 +3,54 @@ package com.gerenciador.api.dto;
 import com.gerenciador.api.model.Produto;
 
 public class ProdutoDTO {
-	
-	private Long id;	
+
+	private Long id;
 	private String nome;
 	private String marca;
 	private double valor;
-	
 
 	public ProdutoDTO() {
-		
-}
 
-public ProdutoDTO (Produto entity) {
-	this.id = entity.getId();
-	this.nome = entity.getNome();
-	this.marca = entity.getMarca();
-	this.valor = entity.getValor();
-		
-}
+	}
 
-public Long getId() {
-	return id;
-}
+	public ProdutoDTO(Produto entity) {
+		this.id = entity.getId();
+		this.nome = entity.getNome();
+		this.marca = entity.getMarca();
+		this.valor = entity.getValor();
 
-public void setId(Long id) {
-	this.id = id;
-}
+	}
 
-public String getNome() {
-	return nome;
-}
+	public Long getId() {
+		return id;
+	}
 
-public void setNome(String nome) {
-	this.nome = nome;
-}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-public String getMarca() {
-	return marca;
-}
+	public String getNome() {
+		return nome;
+	}
 
-public void setMarca(String marca) {
-	this.marca = marca;
-}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-public double getValor() {
-	return valor;
-}
+	public String getMarca() {
+		return marca;
+	}
 
-public void setValor(double valor) {
-	this.valor = valor;
-}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
 
+	public double getValor() {
+		return valor;
+	}
 
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
 
 }
